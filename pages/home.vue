b4637f561470796ecd3105f64e77ededf983a67d
ChKhasan <chulliyevhasan1499@mail.ru>
2023-05-01 16:47:36 +0500

    last

--- pages/home.vue
+++ pages/home.vue
@@ -1,58 +1,142 @@
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
 
-interface posts1 {
-  post?: object;
-  subtitle: object;
-  desc: object;
-  instagramm: string | null;
-  telegram: string | null;
-}
-definePageMeta({
-  pageTransition: {
-    name: "page",
-  },
-});
 export default defineComponent({
   setup() {
-    const count = ref<number>(0);
+    const count = ref(0);
     const postsData = ref({
       post: {},
     });
-    const name = ref<number>(0);
-    const text = ref<string>("");
-    const post = ref<any>({});
-    const id = ref(23);
+    const slides = ref(
+      Array.from({ length: 10 }, () => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        // Figure out contrast color for font
+        const contrast = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "black" : "white";
 
+        return { bg: `rgb(${r}, ${g}, ${b})`, color: contrast };
+      })
+    );
+    // definePageMeta({
+    //   pageTransition: {
+    //     name: "page",
+    //   },
+    //   layout: "custom",
+    // });
+    const name = ref(0);
+    const text = ref("");
+    const post = ref({});
+    const id = ref(23);
+    const router = useRouter();
+    // router.replace({ query: { page: 1 } });
     const { $axios, $good } = useNuxtApp();
     const countUp = () => {
       count.value++;
     };
+    onMounted(() => {
+      // name.value = 12;
+      router.push({
+        path: "/home",
+        query: { streamer: "1" },
+      });
+    });
+    name.value = 123;
+
     const url = computed(() => {
       return `https://api.safarpark.uz/api/posts`;
     });
-    const { pending, data, error } = useFetch<any>(url);
+    const { pending, data, error } = useFetch(url);
 
     // onMounted(async () => {
     //   post.value = await $axios.get(`https://api.safarpark.uz/api/posts/${id.value}`);
     //   console.log("good", post);
     // });
     watch(name, (newVal, lastVal) => {});
-    return { count, countUp, postsData, pending, data, id, error, text, post };
+    return {
+      count,
+      countUp,
+      postsData,
+      pending,
+      data,
+      id,
+      error,
+      text,
+      post,
+      slides,
+      name,
+    };
   },
 });
 </script>
 <template>
-  <div v-if="error || !data">
+  <div>
+    <Swiper
+      class="swiper-cards"
+      :width="440"
+      :modules="[SwiperAutoplay, SwiperEffectCards]"
+      :slides-per-view="1"
+      :loop="true"
+      :effect="'cards'"
+      :autoplay="{
+        delay: 1000,
+        disableOnInteraction: true,
+      }"
+    >
+      <SwiperSlide
+        v-for="(slide, idx) in slides"
+        :key="idx"
+        :style="`background-color: ${slide.bg}; color: ${slide.color}`"
+      >
+        {{ idx }}
+      </SwiperSlide>
+    </Swiper>
+    <Swiper
+      :height="300"
+      :modules="[SwiperAutoplay, SwiperEffectCreative]"
+      :slides-per-view="1"
+      :loop="true"
+      :effect="'creative'"
+      :autoplay="{
+        delay: 1000,
+        disableOnInteraction: true,
+      }"
+      :creative-effect="{
+        prev: {
+          shadow: false,
+          translate: ['-20%', 0, -1],
+        },
+        next: {
+          translate: ['100%', 0, 0],
+        },
+      }"
+    >
+      <SwiperSlide
+        v-for="(slide, idx) in slides"
+        :key="idx"
+        :style="`background-color: ${slide.bg}; color: ${slide.color}`"
+      >
+        {{ idx }}
+      </SwiperSlide>
+
+      <!-- useSwiper() within a swiper instance -->
+      <SwiperControls />
+    </Swiper>
+    <h1>Good name {{ name }}</h1>
     <h1>Good error</h1>
-  </div>
-  <div v-else>
-    <h1>{{ pending }}</h1>
-    <h1>{{ error }}</h1>
-    <h4>{{ data }}</h4>
-    <input type="text" v-model="text" />
-    <a-button type="primary" @click="countUp">Count {{ $good(text) }}</a-button>
-    <nuxt-link to="/">Home</nuxt-link>
+    <h1>Good error</h1>
+    <div v-if="error || !data">
+      <h1>Good error</h1>
+      <h1>Good error</h1>
+      <h1>Good error</h1>
+      <h1>Good error</h1>
+      <h1>Good error</h1>
+    </div>
+    <div v-else>
+      <input type="text" v-model="text" />
+      <a-button type="primary" @click="countUp">Count {{ $good(text) }}</a-button>
+      <nuxt-link to="/">Home</nuxt-link>
+    </div>
   </div>
 </template>
 
@@ -65,5 +149,36 @@ export default defineComponent({
 .page-leave-to {
   opacity: 0;
   filter: blur(1rem);
+}
+.rotate-enter-active,
+.rotate-leave-active {
+  transition: all 0.4s;
+}
+.rotate-enter-from,
+.rotate-leave-to {
+  opacity: 0;
+  transform: rotate3d(1, 1, 1, 15deg);
+}
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  height: 20vh;
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: "Roboto", sans-serif;
+}
+.swiper-wrapper {
+  min-width: 100vh;
+  width: 100vh;
+}
+.swiper-cards {
+  width: 240px;
+  height: 240px;
+}
+.swiper-cards .swiper-slide {
+  border-radius: 6px;
+  border: 1px solid black;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,13 @@
-<template>
-  <!-- <div v-if="error || !data">
-    <h1>asdas</h1>
-  </div> -->
-  <div>
-    <!-- <h1>Good {{ pending }}</h1> -->
-    <nuxt-link to="/about">nuxt link</nuxt-link>
-    <nuxt-link to="/home">Home</nuxt-link>
-    <p>{{ $good("good") }}</p>
-    <pre class="text-left"><code>adsa{{ good }}good</code></pre>
-    <h1>hello{{ hello }}</h1>
-    <button @click="count++">count</button>
-    <!-- <p>{{ data.posts.data[0].id }}</p> -->
-    <!-- <p>Posts {{ posts.posts }}</p> -->
-    Page visits: {{ good }}
-  </div>
-</template>
-
 <script setup lang="ts">
-definePageMeta({
-  pageTransition: {
-    name: "rotate",
-  },
-});
+import { Drag, DropList } from "vue-easy-dnd";
+
+// definePageMeta({
+//   pageTransition: {
+//     name: "page",
+//   },
+//   layout: "custom",
+// });
+const layout = "custom";
 interface news {
   posts: object;
 }
@@ -32,24 +18,97 @@ const news = ref<news>({
     name: "asdsad",
   },
 });
+const items = ref(["a", "b", "c", "d", "e"]);
 const url = computed(() => {
   return `https://api.safarpark.uz/api/posts`;
 });
-
+const onInsert = (event: any) => {
+  items.value.splice(event.index, 0, event.data);
+};
 const count = ref(1);
 const { data: good } = await useFetch(() => `/api/test`);
 const { data: hello } = await useFetch(() => `/api/hello`);
 
 const { pending, data: post, error } = useFetch<any>(url);
 </script>
+<template>
+  <!-- <div v-if="error || !data">
+    <h1>asdas</h1>
+  </div> -->
+  <div>
+    <h1>Good</h1>
+    <h1>Good</h1>
+    <h1>Good</h1>
+    <h1>Good</h1>
+    <h1>Good</h1>
+    <h1>Good</h1>
+    <nuxt-link to="/about">nuxt link</nuxt-link>
+    <nuxt-link to="/home">Home</nuxt-link>
+    <p>{{ $good("good") }}</p>
+    <div class="row good">
+      <div class="col-3">
+        <drag v-for="n in [1, 2, 3, 4, 5]" :data="n" class="item" :key="n">{{ n }}</drag>
+      </div>
+      <drop-list
+        :items="items"
+        class="list"
+        @insert="onInsert"
+        @reorder="$event.apply(items)"
+      >
+        <template v-slot:item="{ item }">
+          <drag class="item" :key="item">{{ item }}</drag>
+        </template>
+        <template v-slot:feedback="{ data }">
+          <div class="item feedback" :key="data">{{ data }}</div>
+        </template>
+      </drop-list>
+    </div>
+    <pre class="text-left"><code>adsa{{ good }}good</code></pre>
+    <h1>hello{{ hello }}</h1>
+    <button @click="count++">count</button>
+    <!-- <p>{{ data.posts.data[0].id }}</p> -->
+    <!-- <p>Posts {{ posts.posts }}</p> -->
+    Page visits: {{ good }}
+  </div>
+</template>
+
+
 <style>
-.rotate-enter-active,
-.rotate-leave-active {
-  transition: all 0.4s;
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
 }
-.rotate-enter-from,
-.rotate-leave-to {
+.page-enter-from,
+.page-leave-to {
   opacity: 0;
-  transform: rotate3d(1, 1, 1, 15deg);
+  filter: blur(1rem);
+}
+.list {
+  border: 1px solid black;
+  margin: 100px auto;
+  width: 200px;
+}
+.item {
+  padding: 20px;
+  margin: 10px;
+  background-color: rgb(220, 220, 255);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.feedback {
+  background-color: rgb(255, 220, 220);
+  border: 2px dashed black;
+}
+
+.drag-image {
+  background-color: rgb(220, 255, 220);
+  transform: translate(-50%, -50%);
+}
+.col-3 {
+  width: 200px;
+}
+.row {
+  display: flex;
 }
 </style>
